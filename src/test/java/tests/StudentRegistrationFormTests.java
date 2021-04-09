2b73c7a99cef71868b597aaccfcdb0af3f2eef9e
Oksana Izibekova <oksana.izibekova@mail.com>
2021-04-09 21:35:25 +0300

    added negatve test

--- src/test/java/tests/StudentRegistrationFormTests.java
+++ src/test/java/tests/StudentRegistrationFormTests.java
@@ -89,6 +89,68 @@ public class StudentRegistrationFormTests extends TestBase {
                     text(hobby1),
                     text(picture),
                     text(currentAddress),
+                    text(state + " " + city));
+        });
+    }
+
+    @Test
+    void negativeFillFormTest() {
+        step("Open students registration form", () -> {
+            open("https://demoqa.com/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        });
+
+        step("Fill students registration form", () -> {
+            $("#firstName").setValue(firstName);
+            $("#lastName").setValue(lastName);
+            $("#userEmail").setValue(email);
+            $("#genterWrapper").$(byText(gender)).click();
+            $("#userNumber").setValue(mobile);
+        });
+
+        step("Set date", () -> {
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").find(byText(monthOfBirth)).click();
+            $(".react-datepicker__year-select").selectOptionByValue(yearOfBirth);
+            $(".react-datepicker__day--0" + dayOfBirth).click();
+        });
+
+        step("Set subject", () -> {
+            $("#subjectsContainer").click();
+            $("#subjectsInput").setValue(subject1).pressEnter();
+        });
+
+        step("Set hobbey", () -> {
+            $("#hobbiesWrapper").$(byText(hobby1)).click();
+        });
+
+        step("Upload file", () -> {
+            $("#uploadPicture").uploadFromClasspath(picture);
+        });
+
+        step("Set address", () -> {
+            $("#currentAddress").val(currentAddress);
+            $("#state").scrollTo().click();
+            $("#stateCity-wrapper").$(byText(state)).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText(city)).click();
+        });
+
+        step("Submit form", () -> {
+            $("#submit").click();
+        });
+
+        step("Verify successful form submit", () -> {
+            $(".modal-content").shouldHave(text(firstName),
+                    text(lastName),
+                    text(email),
+                    text(gender),
+                    text(mobile),
+                    text(subject1),
+                    text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth),
+                    text(hobby1),
+                    text(picture),
+                    text(currentAddress),
                     text(state + " error " + city));
         });
     }
